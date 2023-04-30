0052c6e2ea955b23298953ee6e45c3fd32d23ef7
ISAIAS <issuyep@yahoo.com>
2023-04-29 22:40:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+const yourName = "Isaias"
+newHeader.textContent = `${yourName} is the champion`;
